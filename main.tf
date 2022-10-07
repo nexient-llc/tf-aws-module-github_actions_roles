408fb4a395e6723204e9b1670ec658d9f4ec9c13
Brandon Bleau <bbleau@nexient.com>
2022-10-07 16:16:50 -0400

    add condition for sts.amazonaws.com (#2)

--- main.tf
+++ main.tf
@@ -30,6 +30,12 @@ data "aws_iam_policy_document" "assume_role" {
       ]
     }
 
+    condition {
+      test = "StringLike"
+      variable = "token.actions.githubusercontent.com:aud"
+      values = ["sts.amazonaws.com"]
+    }
+
     dynamic "condition" {
       for_each = var.conditions
 
